97c64fbed6dd5de0d5cf951ed6198f73da111695
Ivan Chen <ivan4u@gmail.com>
2015-11-21 20:36:18 +0800

    improve it.

--- src/main/java/org/springside/examples/quickstart/config/ShiroConfiguration.java
+++ src/main/java/org/springside/examples/quickstart/config/ShiroConfiguration.java
@@ -19,12 +19,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ShiroConfiguration{
+public class ShiroConfiguration {
 
     private static Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
 
     @Bean(name = "securityManager")
-    @Autowired
     public DefaultWebSecurityManager getDefaultWebSecurityManager(AuthorizingRealm realm) {
         DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
         dwsm.setRealm(realm);
@@ -33,15 +32,13 @@ public class ShiroConfiguration{
     }
 
     @Bean(name = "realm")
-    @Autowired
-    public AuthorizingRealm getShiroRealm(@Qualifier("accountService") AccountService accountService) {
-        ShiroDbRealm shiroDbRealm=new ShiroDbRealm();
+    public AuthorizingRealm getShiroRealm(AccountService accountService) {
+        ShiroDbRealm shiroDbRealm = new ShiroDbRealm();
         shiroDbRealm.setAccountService(accountService);
         return shiroDbRealm;
     }
 
     @Bean(name = "shiroFilter")
-    @Autowired
     public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean
@@ -50,11 +47,11 @@ public class ShiroConfiguration{
         shiroFilterFactoryBean.setSuccessUrl("/");
         filterChainDefinitionMap.put("/login", "authc");
         filterChainDefinitionMap.put("/logout", "logout");
-        filterChainDefinitionMap.put("/static/**","anon");
-        filterChainDefinitionMap.put("/api/**","anon");
-        filterChainDefinitionMap.put("/register/**","anon");
-        filterChainDefinitionMap.put("/admin/**","roles[admin]");
-        filterChainDefinitionMap.put("/**","user");
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/api/**", "anon");
+        filterChainDefinitionMap.put("/register/**", "anon");
+        filterChainDefinitionMap.put("/admin/**", "roles[admin]");
+        filterChainDefinitionMap.put("/**", "user");
 
         shiroFilterFactoryBean
                 .setFilterChainDefinitionMap(filterChainDefinitionMap);
@@ -81,13 +78,11 @@ public class ShiroConfiguration{
     }
 
     @Bean
-    @Autowired
     public AuthorizationAttributeSourceAdvisor getAuthorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
         aasa.setSecurityManager(securityManager);
         return new AuthorizationAttributeSourceAdvisor();
     }
-
 
 
 }
