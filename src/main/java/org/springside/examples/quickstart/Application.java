8333f94cfa1c2744d320d17091972f81a7001f3b
Ivan Chen <ivan4u@gmail.com>
2015-11-25 15:14:13 +0800

    improve it.

--- src/main/java/org/springside/examples/quickstart/Application.java
+++ src/main/java/org/springside/examples/quickstart/Application.java
@@ -21,9 +21,7 @@ import java.io.*;
 public class Application {
 
     public static void main(String[] args) throws FileNotFoundException {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.run(args);
-
+        new SpringApplication(Application.class).run(args);
     }
 
     @Bean
